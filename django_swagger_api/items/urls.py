5e5ca1700fb4526067facb97226460eae509020e
ParvathyReghunath <parvathyreghunath.mec@gmail.com>
2024-10-24 14:42:50 +0530

    Added urls and view.py

---
+++ django_swagger_api/items/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from .views import ItemListCreate,ItemDetail
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('items/',ItemListCreate.as_view(),name='item_list'),
+    path('items/<int:pk>',ItemDetail.as_view(),name='item_detail'),
+
+]
--- django_swagger_api/items/views.py
+++ django_swagger_api/items/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from .models import Item
+from .serializers import Itemserializer
 # Create your views here.
+
+
+class ItemListCreate(generics.ListCreateAPIView):
+    queryset = Item.objects.all()
+    serializer_class = Itemserializer
+
+class ItemDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset =Item.objects.all()
+    serializer_class =Itemserializer
