5c27811c4632a20cf2387ca0eb819b229aacec57
ParvathyReghunath <parvathyreghunath.mec@gmail.com>
2024-10-24 12:24:03 +0530

    Added class items in models.py

--- django_swagger_api/django_swagger_api/settings.py
+++ django_swagger_api/django_swagger_api/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'items',
+    'rest_framework',        #PR
+    'drf_yasg',                 #PR
+
 ]
 
 MIDDLEWARE = [
